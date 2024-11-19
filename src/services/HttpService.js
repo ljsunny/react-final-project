ad6056886392f32d28346b6a8b0ece40d1cc81ff
ljsunny <js961223@naver.com>
2024-11-18 20:30:45 -0800

    fix path

--- src/services/HttpService.js
+++ src/services/HttpService.js
@@ -1,7 +1,7 @@
 import http from "../http-common";
 
 class HttpService{
-    get(fileName,path=""){
+    get(fileName,path=`${process.env.PUBLIC_URL}`){
         return http.get(`${path}/${fileName}`);
     }
 }
