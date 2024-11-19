da0080dcb0e2a0ffb4c023e0c5ce0de20187a53b
ljsunny <js961223@naver.com>
2024-11-18 20:24:31 -0800

    fix path

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
   return (
     <div className="wrap">
       <SnowEffect/>
-      <BrowserRouter basename="/react-final-project">>
+      <BrowserRouter basename="/react-final-project/">
         <Routes>
           <Route path="/" element={<Master isLogin={loggedInUserId}/>}>
             {loggedInUserId ? 
